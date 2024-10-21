fba0c6c1212d4c310228c8c9c9e915634444dc2a
PrajwalMadikai <133801108+PrajwalMadikai@users.noreply.github.com>
2024-10-21 17:05:06 +0530

    google auth updated code

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,56 +1,51 @@
-const passport=require('passport')
-const googleStrategy=require('passport-google-oauth20').Strategy;
-const User=require('../model/userSchema')
-const env=require('dotenv').config()
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../model/userSchema');
+require('dotenv').config();
 
 passport.use(new googleStrategy({
-    clientID:process.env.CLIENT_ID,
-    clientSecret:process.env.CLIENT_SECRET,
-    callbackURL:'https://trendview.tech/auth/google/callback',
-    
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'https://trendview.tech/auth/google/callback', // Updated callback URL
+
 },
-async (accessToken,refreshToken,profile,done)=>{
+async (accessToken, refreshToken, profile, done) => {
     try {
-          let user=await User.findOne({googleId:profile.id})
+        let user = await User.findOne({ googleId: profile.id });
 
-          if(user)
-          {
-            return done(null,user)
-          }else{
-            user=new User({
-                firstName:profile.displayName,
-                email:profile.emails[0].value,
-                googleId:profile.id,
-                isBlock:false,
-                isAdmin:false
-            })
-             
-            await user.save()
-            return done(null,user)
-          }   
+        if (user) {
+            return done(null, user);
+        } else {
+            user = new User({
+                firstName: profile.displayName,
+                email: profile.emails[0].value,
+                googleId: profile.id,
+                isBlock: false,
+                isAdmin: false
+            });
+
+            await user.save();
+            return done(null, user);
+        }
     } catch (error) {
-        
-        return done(error,null)
-        
-    } 
+        return done(error, null);
+    }
 }
-)) 
- 
-//to store into session
-passport.serializeUser((user,done)=>{
-    done(null,user.id)
-})
+));
 
-//to fetch data from session
-passport.deserializeUser((id,done)=>{
-     User.findById(id)
-     .then((user)=>{
-        done(null,user)
-     })
-     .catch(err=>{
-        console.error("Error in deserialization:",err)
-        done(err,null)
-     })
-})
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-module.exports=passport
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then((user) => {
+        done(null, user);
+    })
+    .catch(err => {
+        console.error("Error in deserialization:", err);
+        done(err, null);
+    });
+});
+
+module.exports = passport;
